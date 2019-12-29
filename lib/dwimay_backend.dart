93d6b95fb309199e114236ed14fba4995c38fda1
firekind <shyamant.achar@gmail.com>
2019-12-29 16:38:40 +0530

    updated export

--- lib/dwimay_backend.dart
+++ lib/dwimay_backend.dart
@@ -1,6 +1,7 @@
 library dwimay_backend;
 
 export 'package:dwimay_backend/models/events_model.dart';
+export 'package:dwimay_backend/models/user_model.dart';
 export 'package:dwimay_backend/widgets/event_loader.dart';
 export 'package:dwimay_backend/widgets/login_widget.dart';
 export 'package:dwimay_backend/widgets/notification_provider.dart';
